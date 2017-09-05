32050fc173232abd98f6eb7b7d0d5617125ffefb
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2017-09-05 16:30:24 -0300

    More server tests

--- test/server.js
+++ test/server.js
@@ -1,11 +1,29 @@
 import { server } from "../main/main";
 import { get } from "http";
+import * as assert from "assert";
 
 describe("server", () => {
     it("should start server at port 9000", (done) => {
         server.start();
-        get("http://localhost:9000", (response) => {
-            server.stop();
+        get("http://localhost:9000", (message) => {
+            done();
+        });
+    });
+
+    it("should return application-json", (done) => {
+        get("http://localhost:9000", (message) => {
+            assert.equal(message.headers["content-type"], "application/json; charser=utf-8");
+            done();
+        });
+    });
+    
+    it("should stop server", (done) => {
+        server.stop();
+
+        const req = get("http://localhost:9000");
+
+        req.on("error", (e) => {
+            assert.equal(e.code, "ECONNREFUSED");
             done();
         });
     });
